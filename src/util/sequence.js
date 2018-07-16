e2d5fe8599a5f4002ce4bc80dc5f24751d123d33
phamann <patrick@fastly.com>
2018-07-16 14:16:19 +0100

    Replace Promise sequence utility with a new working implementation

--- src/util/sequence.js
+++ src/util/sequence.js
@@ -1,16 +1,5 @@
 export default function sequence(tasks) {
-  const results = [];
-
-  function push(prom) {
-    return prom().then(res => {
-      if (res) results.push(res);
-      return res;
-    });
-  }
-
-  return tasks
-    .reduce((current, next) => {
-      return current.then(push(next));
-    }, Promise.resolve())
-    .then(() => results);
+  return tasks.reduce((prom, task) => {
+    return prom.then(results => task().then(result => [...results, result]));
+  }, Promise.resolve([]));
 }
