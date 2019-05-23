b7b826f3efb6942b8e50b0410c1ce9f488bf8584
phamann <patrick@fastly.com>
2019-05-23 11:38:55 +0100

    Initial v2.0.0 commit, preparing repo for rewrite

--- src/lib/performance-observer.js
+++
@@ -1,6 +0,0 @@
-/* global PerformanceObserver */
-import PollingPerformanceObserver from "./performance-observer/observer";
-const isSupported =
-  "PerformanceObserver" in window && typeof PerformanceObserver === "function";
-
-export default (isSupported ? PerformanceObserver : PollingPerformanceObserver);
--- src/util/sequence.js
+++
@@ -1,5 +0,0 @@
-export default function sequence(tasks) {
-  return tasks.reduce((prom, task) => {
-    return prom.then(results => task().then(result => [...results, result]));
-  }, Promise.resolve([]));
-}
