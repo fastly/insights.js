0d10edf5385f583f0e260520396f045c44c2486f
phamann <patrick@fastly.com>
2019-05-28 20:47:31 +0100

    Add templated license banner to top of each emitted chunk via webpack

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,8 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+
+const webpack = require('webpack');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
+function getLicenseWithDate() {
+   const license = fs.readFileSync(path.resolve(__dirname, 'LICENSE'), { encoding: 'utf8'});
+   const now = Date.now();
+   const year = new Date(now).getFullYear();
+   const date = new Date(now).toISOString();
+   return license.replace('[year]', year).replace('[date]', date);
+}
+
 module.exports = merge(common, {
    mode: 'production',
+   plugins: [
+      new webpack.BannerPlugin({
+         banner: getLicenseWithDate()
+      })
+   ],
    output: {
     publicPath: '/static/'
    }
