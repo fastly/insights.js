f5ce75e5b11cf9a5b3b5837e838781bcfa19398d
Peter Banka <pbanka@fastly.com>
2019-06-04 14:05:38 -0700

    ➕➖ switching to lighter-weight deps

--- src/worker.ts
+++ src/worker.ts
@@ -1,11 +1,14 @@
-import "whatwg-fetch";
+import "unfetch/polyfill";
+import "native-promise-only";
 
 interface Response {
   json(): Promise<Config>;
 }
 
 export function init(): Promise<Config> {
+  // FIXME; Use real API location, whatever that is
   return fetch("https://api.fastly.com/api/url").then(
     (res: Response): Promise<Config> => res.json()
   );
+  // TODO: deal with errors
 }
