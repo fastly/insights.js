e133c4ade0442e59b465673c954f865a8cb12951
phamann <patrick@fastly.com>
2019-06-14 15:47:18 +0100

    Refactor and rename chooseRenormalize to accept and return array of TaskData instead of weight number array.

--- src/lib/chooseRandomTasks.ts
+++ src/lib/chooseRandomTasks.ts
@@ -1,3 +1,5 @@
+import assign from "../util/assign";
+
 function mean(values: number[]): number {
   return values.reduce((memo: number, i): number => memo + i) / values.length;
 }
@@ -7,54 +9,54 @@ function mean(values: number[]): number {
  * domain of the probability distribution. Then we will "walk" the probability mass
  * until we exceed the generated value
  */
-function basicChooseOne(probs: number[], mass: number): number {
+function basicChooseOne(tasks: TaskData[], mass: number): number {
   const r = mass * Math.random();
   let i = 0;
-  let s = probs[0];
+  let s = tasks[0].weight;
   while (s < r) {
     i += 1;
-    s += probs[i];
+    s += tasks[i].weight;
   }
   return i;
 }
 
-function chooseRenormalize(probs: number[], k: number): number[] {
+function chooseRenormalize(tasks: TaskData[], k: number): TaskData[] {
   //  If we have more work slots than candidates in the probability distribution,
   //  we return them all
-  if (k >= probs.length)
-    return Array(probs.length)
-      .fill(null)
-      .map((x: null, index): number => index);
+  if (k >= tasks.length) {
+    return tasks;
+  }
 
   // Since we are mutating the probabilities vector, we copy it first
-  const p = [...probs];
+  const t = tasks.map((t): TaskData => assign({}, t));
 
   // This assumes that the probability vector is normalized. If it is not, it
   // should be changed to the sum of the weights
-  let remaining = probs.reduce((memo, prob): number => memo + prob);
+  let remaining = tasks
+    .map((t): number => t.weight)
+    .reduce((memo, task): number => memo + task);
 
   // The weight of an additional "ghost" element that we will use to
   // inhibit running tasks that should have low probability
-  const dampeningFactor = Math.min(mean(probs), 0.05);
+  const probs = tasks.map((t): number => t.weight);
+  const dampeningFactor = Math.min(mean(probs), 0.05 * remaining);
   const choices = [];
 
   for (let j = 0; j < k; j++) {
     // Perform choice adding an additional "ghost" element that can be tweaked
     // to control execution eagerness
-    const i = basicChooseOne(
-      p.concat([dampeningFactor]),
-      remaining + dampeningFactor
-    );
+    const d = ({ weight: dampeningFactor } as any) as TaskData;
+    const i = basicChooseOne(t.concat([d]), remaining + dampeningFactor);
 
     // If we choose a _real_ task, instead of the _ghost_ task, execute the logic
     // inside the conditional
-    if (i < p.length) {
-      // Eliminate chosen element from distribution and renormalize
-      remaining -= p[i];
-      p[i] = 0;
-
+    if (i < t.length) {
       // Add selected element to the selection set
-      choices.push(i);
+      choices.push(assign({}, t[i]));
+
+      // Eliminate chosen element from distribution and renormalize
+      remaining -= t[i].weight;
+      t[i].weight = 0;
     }
   }
 
