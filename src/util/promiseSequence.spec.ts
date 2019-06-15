d264fa3ef778db19a73b191db4b2d78113a1239b
phamann <patrick@fastly.com>
2019-06-15 17:38:25 +0100

    Add utility to sequentially process and array of promise generating functions.

---
+++ src/util/promiseSequence.spec.ts
@@ -0,0 +1,71 @@
+import sequence from "./promiseSequence";
+
+describe("promiseSequence", (): void => {
+  it("should invoke each promise generating function in sequence", (): Promise<
+    void
+  > => {
+    const mockFuncOne = jest.fn().mockReturnValue(Promise.resolve(1));
+    const mockFuncTwo = jest.fn().mockReturnValue(Promise.resolve(2));
+    const mockFuncThree = jest.fn().mockReturnValue(Promise.resolve(3));
+
+    const fixture = [mockFuncOne, mockFuncTwo, mockFuncThree];
+
+    const result = sequence(fixture);
+
+    return Promise.resolve()
+      .then(
+        (): Promise<void> => {
+          expect(mockFuncOne).toHaveBeenCalledTimes(1);
+          expect(mockFuncTwo).toHaveBeenCalledTimes(0);
+          expect(mockFuncTwo).toHaveBeenCalledTimes(0);
+          return Promise.resolve();
+        }
+      )
+      .then(
+        (): Promise<void> => {
+          return Promise.resolve();
+        }
+      )
+      .then(
+        (): Promise<void> => {
+          expect(mockFuncOne).toHaveBeenCalledTimes(1);
+          expect(mockFuncTwo).toHaveBeenCalledTimes(1);
+          expect(mockFuncThree).toHaveBeenCalledTimes(0);
+          return Promise.resolve();
+        }
+      )
+      .then(
+        (): void => {
+          expect(mockFuncOne).toHaveBeenCalledTimes(1);
+          expect(mockFuncTwo).toHaveBeenCalledTimes(1);
+          expect(mockFuncThree).toHaveBeenCalledTimes(1);
+        }
+      )
+      .then(
+        (): Promise<any> => {
+          return result;
+        }
+      )
+      .then(
+        (results): void => {
+          expect(results).toEqual([1, 2, 3]);
+        }
+      );
+  });
+
+  it("should continue the sequencing if a promise fails", (): Promise<void> => {
+    const mockFuncOne = jest.fn().mockReturnValue(Promise.resolve(1));
+    const mockFuncTwo = jest.fn().mockReturnValue(Promise.reject(2));
+    const mockFuncThree = jest.fn().mockReturnValue(Promise.resolve(3));
+
+    const fixture = [mockFuncOne, mockFuncTwo, mockFuncThree];
+
+    const result = sequence(fixture);
+
+    return result.then(
+      (results): void => {
+        expect(results).toEqual([1, 3]);
+      }
+    );
+  });
+});
