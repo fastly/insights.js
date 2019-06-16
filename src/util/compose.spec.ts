e00c487bdf1adbc71a586d17135356fdf6faae18
phamann <patrick@fastly.com>
2019-06-16 11:01:26 +0100

    Add compose utility to compose functions

---
+++ src/util/compose.spec.ts
@@ -0,0 +1,15 @@
+import compose from "./compose";
+
+describe("compose", (): void => {
+  it("should compose functions", (): void => {
+    const fn1 = (val: string): string => `fn1(${val})`;
+    const fn2 = (val: string): string => `fn2(${val})`;
+    const fn3 = (val: string): string => `fn3(${val})`;
+    const composedFunction = compose(
+      fn1,
+      fn2,
+      fn3
+    );
+    expect(composedFunction("inner")).toBe("fn1(fn2(fn3(inner)))");
+  });
+});
