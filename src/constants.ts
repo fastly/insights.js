22374df680d08175cd403cd255bc1b7429eb7a62
phamann <patrick@fastly.com>
2019-07-19 15:24:07 +0100

    Fix script src regexp for production allow pre-prod environments

--- src/constants.ts
+++ src/constants.ts
@@ -8,5 +8,5 @@ export const CONFIG_PATH = "/api/v1/config/";
 export const CLIENT_INFO_PATH = "/client-info";
 export const CONFIG_URL = `${CONFIG_HOST}${CONFIG_PATH}`;
 export const SCRIPT_SRC_REGEXP = PRODUCTION
-  ? /$https:\/\/www\.fastly-insights.com/
+  ? /^https:\/\/[a-z]+\.fastly-insights\.com/
   : /\/main/;
