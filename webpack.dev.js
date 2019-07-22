5c14438b54a314e5b4202c41424273126580fb7e
phamann <patrick@fastly.com>
2019-07-22 14:15:26 +0100

    Fix version constant in development mode

--- webpack.dev.js
+++ webpack.dev.js
@@ -16,7 +16,7 @@ module.exports = merge(common, {
   plugins: [
     new webpack.DefinePlugin({
       PRODUCTION: 'false',
-      VERSION: '0.0.0.'
+      VERSION: JSON.stringify('0.0.0')
     })
   ],
   output: {
