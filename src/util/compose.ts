e1f575e815b965a2b66a3359ef571edc9fcc1448
phamann <patrick@fastly.com>
2021-02-10 11:24:21 +0000

    Cleanup repository for v3.0.0

--- src/util/compose.ts
+++
@@ -1,5 +0,0 @@
-/* eslint-disable @typescript-eslint/explicit-function-return-type */
-const compose = <R>(fn1: (a: R) => R, ...fns: ((a: R) => R)[]) =>
-  fns.reduce((prevFn, nextFn) => value => prevFn(nextFn(value)), fn1);
-
-export default compose;
