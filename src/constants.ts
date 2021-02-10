228e74f8a7192b1cc98351389f555979d3a6e5f5
phamann <patrick@fastly.com>
2021-02-10 12:12:27 +0000

    Large refactor of application to use Open Insights.

---
+++ src/constants.ts
@@ -0,0 +1,3 @@
+export const CONFIG_HOST = "https://fastly-insights.com";
+export const CONFIG_PATH = "/api/v1/config/";
+export const SCRIPT_SRC_REGEXP = /^https?:\/\/(192\.168\.1\.20|localhost|[a-z]+\.fastly-insights\.com)/;
